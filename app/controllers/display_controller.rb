253709757ba5b90a9ce6ba2cce26ebe327852213
Mitsuru Tachibana <mitsuru.t.3260@gmail.com>
2022-01-06 16:49:48 +0900

    visualizeメソッドの修正

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -17,37 +17,53 @@ class DisplayController < ApplicationController
                    recs1_count
                  end
     @class_name = params[:class_name]
-    @recs_arr = return_recs_arr(arr_length, recs1, recs2)
     @recs1_last_id = recs1["record#{recs1_count - 1}"]['0']['id']
     @recs2_last_id = recs2["record#{recs2_count - 1}"]['0']['id']
+    @recs_arr = return_recs_arr(arr_length, recs1, @recs1_last_id, recs2, @recs2_last_id)
   end
 
-  # rubocop:disable Style/For
-  def return_recs_arr(arr_length, recs1, recs2)
+  # rubocop:disable all
+  def return_recs_arr(arr_length, recs1, recs1_last_id, recs2, recs2_last_id)
     recs_arr = []
     recs1_index = 0
     recs2_index = 0
+    recs1_current_id = 0
+    recs2_current_id = 0
     for i in 0..arr_length - 1
       recs1_current = recs1["record#{recs1_index}"]
       recs2_current = recs2["record#{recs2_index}"]
+      # binding.pry
       if recs1_current.nil?
         recs_arr << [nil, recs2_current]
+        # binding.pry
         recs2_index += 1
       elsif recs2_current.nil?
         recs_arr << [recs1_current, nil]
+        # binding.pry
         recs1_index += 1
-      elsif recs1_current['0']['id'] == recs2_current['0']['id']
-        recs_arr << [recs1_current, recs2_current]
-        recs1_index += 1
-        recs2_index += 1
-      elsif recs1_current['0']['id'] > recs2_current['0']['id']
-        recs_arr << [nil, recs2_current]
-        recs2_index += 1
+      else
+        recs1_current_id = recs1_current['0']['id']
+        recs2_current_id = recs2_current['0']['id']
+        # binding.pry
+        if recs1_current_id == recs2_current_id
+          recs_arr << [recs1_current, recs2_current]
+          recs1_index += 1
+          recs2_index += 1
+          # binding.pry
+        elsif recs1_current_id > recs2_current_id
+          recs_arr << [nil, recs2_current]
+          recs2_index += 1
+          # binding.pry
+        elsif recs1_current_id < recs2_current_id
+          recs_arr << [recs1_current, nil]
+          recs1_index += 1
+          # binding.pry
+        end
       end
     end
     recs_arr
   end
-  # rubocop:enable Style/For
+  # rubocop:enable all
 
   def display_params
     params.permit(:class_name, records: {}, records1: {}, records2: {})
